9ce31b16fd32657d082c960d48a7ba9c58a47ae5
Simon Haisz <simonhaisz@gmail.com>
2021-12-03 01:31:34 -0500

    Complete day 3 puzzle 1 by processing the input file

--- day_03_puzzle_01/src/main.rs
+++ day_03_puzzle_01/src/main.rs
@@ -1,5 +1,26 @@
 mod power_consumption;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use power_consumption::PowerConsumption;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./day_03_puzzle_01/input.txt")?;
+    let lines = BufReader::new(file).lines();
+
+    let mut power = PowerConsumption::new();
+    for line in lines {
+        if let Ok(entry) = line {
+            if entry.trim().len() == 0 {
+                // skip any rows with no content
+                continue;
+            }
+            power.analyze_entry(&entry.trim());
+        }
+    }
+
+    println!("The submarine's diagnostics give it a gamma rate of {} and an epsilon rate of {}", power.gamme_rate(), power.epsilon_rate());
+    println!("Multiplied together that gives {}", power.gamme_rate() * power.epsilon_rate());
+
+    Ok(())
 }
