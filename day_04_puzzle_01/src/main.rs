8e26834ee85bde81f6294c67a4723b1f6d42eb23
Simon Haisz <simonhaisz@gmail.com>
2021-12-04 16:30:27 -0500

    Rig the bingo game using the input data

--- day_04_puzzle_01/src/main.rs
+++ day_04_puzzle_01/src/main.rs
@@ -1,5 +1,48 @@
 mod bingo;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+use crate::bingo::{BingoGame, BingoBall, BingoCard};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./day_04_puzzle_01/input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut game = BingoGame::new();
+
+    let mut current_card = None;
+
+    for (i, line) in reader.lines().enumerate() {
+        if let Ok(line) = line {
+            if line.trim().len() == 0 {
+                // skip empty lines
+                continue;
+            }
+            if i == 0 {
+                let ball = BingoBall::new(line.trim());
+                game.setup_ball(ball);
+            } else {
+                if current_card.is_none() {
+                    current_card = Some(BingoCard::new());
+                }
+                if let Some(c) = &mut current_card {
+                    c.load_row(line.trim());
+                    if c.is_full() {
+                        game.add_card(current_card.take().unwrap());
+                    }
+                }
+            }
+        }
+    }
+
+    if let Some(winners) = game.play_game() {
+        for winner in winners.iter() {
+            println!("Calling number {} gave board {} a Bingo with a score of {}", winner.0, winner.1, winner.2);
+        }
+    } else {
+        println!("We ran out of numbers before anyone got a Bingo!");
+    }
+
+    Ok(())
 }
