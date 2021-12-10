ddeb5dd4b3b156950bf5978260b3fad692cd2fbb
Simon Haisz <simonhaisz@gmail.com>
2021-12-10 18:10:59 -0500

    Day 10 part one works against full sample data

--- day_10_puzzle_01/src/main.rs
+++ day_10_puzzle_01/src/main.rs
@@ -3,6 +3,27 @@ extern crate lazy_static;
 
 mod chunk;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use crate::chunk::ChunkChecker;
+
+fn main() -> std::io::Result<()> {
+
+    let file = File::open("./day_10_puzzle_01/input.txt")?;
+    let lines = BufReader::new(file).lines();
+
+    let mut checker = ChunkChecker::new();
+
+    for line in lines {
+        if let Ok(entry) = line {
+            if entry.trim().is_empty() {
+                continue;
+            }
+            checker.parse_line(entry.trim());
+        }
+    }
+
+    println!("{}", checker.syntax_error_score());
+
+    Ok(())
 }
