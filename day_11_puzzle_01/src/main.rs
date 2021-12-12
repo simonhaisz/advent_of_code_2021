6e939aa3d43380c6c0bf36e7be24ad993556098e
Simon Haisz <simonhaisz@gmail.com>
2021-12-12 16:30:03 -0500

    Day 11 part 1 works against sample data

--- day_11_puzzle_01/src/main.rs
+++ day_11_puzzle_01/src/main.rs
@@ -1,5 +1,28 @@
 mod octopus_grid;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use crate::octopus_grid::OctopusGrid;
+
+fn main() -> std::io::Result<()> {
+
+    let file = File::open("./day_11_puzzle_01/input.txt")?;
+    let lines = BufReader::new(file).lines();
+
+    let mut grid = OctopusGrid::new();
+
+    for line in lines {
+        if let Ok(entry) = line {
+            if entry.trim().is_empty() {
+                continue;
+            }
+            grid.add_row(entry.trim().chars()
+            .map(|c| u32::from_str_radix(&c.to_string(), 10).unwrap())
+            .collect::<Vec<u32>>());
+        }
+    }
+
+    println!("{}", grid.compute_flashes(100));
+
+    Ok(())
 }
