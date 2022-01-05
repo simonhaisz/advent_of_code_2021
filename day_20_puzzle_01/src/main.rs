27dc8a9457845c531052405c800539d33d25d742
Simon Haisz <simonhaisz@gmail.com>
2022-01-05 13:05:23 -0500

    20.1 DOESN'T work against test data

--- day_20_puzzle_01/src/main.rs
+++ day_20_puzzle_01/src/main.rs
@@ -3,6 +3,39 @@ extern crate lazy_static;
 
 mod pixel;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use crate::pixel::Image;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./day_20_puzzle_01/input.txt")?;
+    let lines = BufReader::new(file).lines();
+
+    let mut enhancement: Option<String> = None;
+    let mut pixels = vec![];
+    
+    for (index, line) in lines.enumerate() {
+        if line.is_err() {
+            continue;
+        }
+        let line = line.unwrap();
+        if line.trim().is_empty() {
+            continue;
+        }
+        if index == 0 {
+            enhancement = Some(line);
+        } else {
+            pixels.push(line);
+        }
+    }
+
+    let enhancement = enhancement.unwrap();
+
+    let image = Image::new(pixels);
+    let enhanced_image = image.enhance(&enhancement);
+    let enhanced_enhanced_image = enhanced_image.enhance(&enhancement);
+
+    println!("{}", enhanced_enhanced_image.lit_pixel_count());
+
+    Ok(())
 }
